4cf61ba1e85bb86821cdbbc26579191affc0b741
Advys560 <samuel.moralescorrea@inemjose.edu.co>
2026-07-29 12:29:55 -0500

    nose

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+const fecha = new Date().toLocaleString();
+return (
+ <main>
+ <h1>Hola SENA</h1>
+ <p style={{color: 'white'}}>Hola soy Daniel Duque, esta es mi primera pagina web en ReactJS</p>
+ <p>Fecha y hora:{fecha}</p>
+ </main>)}
